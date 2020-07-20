17f970d128f224bdab18a714da16a91af0fc00e8
Ranimarathe <68508241+Ranimarathe@users.noreply.github.com>
2020-07-21 00:25:15 +0530

    Create pythonassignment1.py

---
+++ pythonassignment1.py
@@ -0,0 +1,2 @@
+Python is a computer language where you can easily code. It is a user friendly language and very easy to learn and use.
+Code environment is a place where you can write code, compile the code and see the output of the code. The three types of code environment are text editor, full IDEs and notebook environmemt.
